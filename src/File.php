a5d603a2f52e126de6e93304eeea2f7c5350198a
Richard Lea <chigix@zoho.com>
2015-03-10 00:08:10 +0800

    fix: dev: the code format.
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- src/File.php
+++ src/File.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the io-component package.
+ * 
+ * (c) Richard Lea <chigix@zoho.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Chigi\Component\IO;
 
 use InvalidArgumentException;
